01440aaf1b566f6ecb38d5e6cdbaee4610537d17
rldan <rldan@thoughtworks.com>
2020-07-31 13:07:28 +0800

    modify rule

--- cloud-gateway/src/main/java/top/bujiaban/gateway/config/RouterConfig.java
+++ cloud-gateway/src/main/java/top/bujiaban/gateway/config/RouterConfig.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.filter.factory.RewritePathGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +15,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 @EnableDiscoveryClient
 public class RouterConfig {
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, RewritePathGatewayFilterFactory rewrite) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("toGoogle", r -> r.path("/gogogo")
-                        .uri("https://www.google.com"))
+                .route("toBing", p -> p.path("/gogogo/**")
+                        .filters(f -> f.rewritePath("/gogogo/(?<path>.*)", "/${path}"))
+                        .uri("https://cn.bing.com"))
+                .route("toWeb", p -> p.path("/to/{id}")
+                        .uri("http://127.0.0.1:8080")
+                )
                 .build();
     }
 
